784392415089e76bc69260c87949ed93ba834faf
Dastan Akhmetov <dastan.mvp97@gmail.com>
2021-01-08 14:57:52 +0600

    added new route /add for page refresh

--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
+Route::get('/add', [App\Http\Controllers\HomeController::class, 'index']);
 Auth::routes();
